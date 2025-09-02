9911022fc7d41d1da0f236c2d3883bc506f0b46a
Bello Oluwadamilare <darrex373@gmail.com>
2025-09-02 15:10:01 +0100

    worked on methods in in classes

--- classes.py
+++ classes.py
@@ -48,13 +48,13 @@
 # print(Tope.airtime)
     
 # Defining Attributes of a student
-class Student:
-    def __init__(self, name, course, level, state_of_origin):
-        self.name = name
-        self.course = course
-        self.level = level
-        self.state_of_origin = state_of_origin
-        self.cgpa = 0.0
+# class Student:
+#     def __init__(self, name, course, level, state_of_origin):
+#         self.name = name
+#         self.course = course
+#         self.level = level
+#         self.state_of_origin = state_of_origin
+#         self.cgpa = 0.0
 
 
 # Ayomide = Student("Ayomide Bello", "Linguistics", 100, "Ogun State")
@@ -66,23 +66,48 @@ class Student:
 
 # Types of Attributes
 # Instance Attributes - unique to each objects
-student1 = Student("John Paul", "Hospitality", 500, "Ebonyi State")
-student2 = Student("Abel King", "Management", 500, "Anambra State")
+# student1 = Student("John Paul", "Hospitality", 500, "Ebonyi State")
+# student2 = Student("Abel King", "Management", 500, "Anambra State")
 
-print(student1.name)
-print(student2.course)
+# print(student1.name)
+# print(student2.course)
 
-# Class Attributes
+# # Class Attributes
+# class Student:
+#     university = "Federal University of Agriculture Abeokuta"
+
+#     def __init__(self, name, course):
+#         self.name = name
+#         self.course = course
+
+# print(Student.university)
+# print(student1.university)
+# print(student2.university)
+
+# Methods: they define actions object can perform. they are like a function
 class Student:
-    university = "Federal University of Agriculture Abeokuta"
-
-    def __init__(self, name, course):
+    def __init__(self, name, course, level):
         self.name = name
         self.course = course
+        self.level = level
+        self.cgpa = 0.0
+        self.fees_paid = False
 
-print(Student.university)
-print(student1.university)
-print(student2.university)
+# Method: Action the student can do
+def pay_school_fees(self):
+    self.fees_paid = True
+    return f"{self.name} has paid school fees for {self.level} level"
 
-# Methods
+# Method: Another action
+def register_courses(self):
+    if self.fees_paid:
+        return f"{self.name} has registered courses for {self.course} "
+    else:
+        return f"{self.name} must pay school fees first!"
 
+# Method Calculates CGPA
+def calculate_cgpa(self, grades):
+    if grades:
+        self.cgpa = sum(grades) / len(grades)
+        return f"{self.name}'s CGPA is now {self.cgpa:.2f}"
+    return "No grades provided"
